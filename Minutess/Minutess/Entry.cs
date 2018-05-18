0f6985820fb1b92acb5d86291f71b35804c0fcdd
Juan Gerardo González Esparza <jugerardo@hotmail.com>
2018-05-18 09:19:54 -0500

    Entry class

---
+++ Minutess/Minutess/Entry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minutess
+{
+    public class Entry
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public override string ToString()
+        {
+            return Title + " " + CreateDate;
+        }
+    }
+}

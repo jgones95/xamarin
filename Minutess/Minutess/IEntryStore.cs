9a83f85844965a98cb9cd22b0de0f78c6f4bae16
Juan Gerardo González Esparza <jugerardo@hotmail.com>
2018-05-18 09:29:53 -0500

    Code the storage interface

---
+++ Minutess/Minutess/IEntryStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+namespace Minutess
+{
+    public interface IEntryStore
+    {
+        Task<List<Entry>> ReadAsync();
+        Task WriteAsync(Entry entry);
+        Task DeleteAsync(Entry entry);
+
+    }
+}

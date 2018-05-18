460569cc581c65b93ab9f4cf1e0100fc8a3dd26b
Juan Gerardo González Esparza <jugerardo@hotmail.com>
2018-05-18 11:25:50 -0500

    Specify an Entry's Visualization in a ListView

--- Minutess/Minutess/MockDataExtensionMethods.cs
+++ Minutess/Minutess/MockDataExtensionMethods.cs
@@ -9,7 +9,11 @@ namespace Minutess
         public static void LoadMockData(this IEntryStore store)
         {
             var a = new Entry() { Title = "Sprint Planning Meeting", Content = "1. Scope" };
+            var b = new Entry() { Title = "Sprint Planning Meeting", Content = "1. Scope" };
+            var c = new Entry() { Title = "Sprint Planning Meeting", Content = "1. Scope" };
             store.WriteAsync(a).Wait();
+            store.WriteAsync(b).Wait();
+            store.WriteAsync(c).Wait();
         }
     }
 }
